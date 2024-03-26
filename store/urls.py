21899daf2c938689a478455f59a1875e2d6238fc
Vlad S <vlads1976@gmail.com>
2024-03-26 17:39:47 +0100

    added login and logout user.

--- store/urls.py
+++ store/urls.py
@@ -5,5 +5,7 @@ from store import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("about/", views.about, name="about"),
-
+    path("login/", views.login_user, name="login"),
+    path("logout/", views.logout_user, name="logout"),
+    
 ]
--- store/views.py
+++ store/views.py
@@ -1,10 +1,36 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import redirect, render
+
 from .models import Product
 
 
 def home(request):
     products = Product.objects.all()
-    return render(request, 'store/index.html', {'products': products})
+    return render(request, "store/index.html", {"products": products})
+
 
 def about(request):
-    return render(request, 'store/about.html')
+    return render(request, "store/about.html")
+
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Login Successfully")
+            return redirect("home")
+        else:
+            messages.error(request, "Invalid username or password")
+            return redirect("login")
+    else:
+        return render(request, "store/login.html")
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "Logout Successfully")
+    return redirect("home")
