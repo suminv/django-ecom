b28db50f8fd183dc50870255bfcf06fcd893a0eb
Vlad S <vlads1976@gmail.com>
2024-03-29 17:01:28 +0100

    add item to cart using session.

--- cart/views.py
+++ cart/views.py
@@ -1,13 +1,40 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404, render
+
+from store.models import Product
+
+from .cart import Cart
+
 
 def cart_summary(request):
     return render(request, "cart/cart_summary.html")
 
+
 def cart_add(request):
-    return render(request, "cart/cart_add.html")
+    # Get the cart
+    cart = Cart(request)
+    # test for POST
+    if request.POST.get("action") == "post":
+        # Get stuff
+        product_id = int(request.POST.get("product_id"))
+
+        # lookup product in DB
+        product = get_object_or_404(Product, id=product_id)
+
+        # Save to session
+        cart.add(product=product)
+
+        # Return resonse
+        response = JsonResponse({"Product Name: ": product.name})
+
+        messages.success(request, ("Product Added To Cart..."))
+        return response
+
 
 def cart_delete(request):
     return render(request, "cart/cart_delete.html")
+
 
 def cart_update(request):
     return render(request, "cart/cart_update.html")
