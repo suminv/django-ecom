00d3a1d82ceea628f45b02190addebb46cfe08d8
Vlad S <vlads1976@gmail.com>
2024-05-01 16:22:08 +0200

    added to admin panel extended Order

--- payment/admin.py
+++ payment/admin.py
@@ -1,3 +1,4 @@
+from dataclasses import fields
 from django.contrib import admin
 from .models import ShipppingAddres, Order, OrderItem
 
@@ -5,3 +6,21 @@ from .models import ShipppingAddres, Order, OrderItem
 admin.site.register(ShipppingAddres)
 admin.site.register(Order)
 admin.site.register(OrderItem)
+
+# Create OrderItem Inline
+class OrderItemInline(admin.StackedInline):
+    model = OrderItem
+    extra = 0
+
+# Extend Order Model
+class OrderAdmin(admin.ModelAdmin):
+    model = Order
+    readonly_fields = ['date_ordered']
+    fields = ['full_name', 'email', 'shipping_address','amount_paid', 'date_ordered', 'shipped']
+    inlines = [OrderItemInline]
+
+# Unregister the old model
+admin.site.unregister(Order)
+
+#Re - register our Order and OrderAdmin
+admin.site.register(Order, OrderAdmin)
